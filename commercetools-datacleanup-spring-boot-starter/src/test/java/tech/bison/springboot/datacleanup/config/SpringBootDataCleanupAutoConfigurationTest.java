23054c92e0d3aff84dbbf5f08068253f266b1647
Marco Studer <marco.studer@gmx.net>
2025-05-25 10:58:18 +0200

    replace mockito bean
    and setup instrumentation for inline mocking
    
    Signed-off-by: Marco Studer <marco.studer@bison-group.com>

--- commercetools-datacleanup-spring-boot-starter/src/test/java/tech/bison/springboot/datacleanup/config/SpringBootDataCleanupAutoConfigurationTest.java
+++ commercetools-datacleanup-spring-boot-starter/src/test/java/tech/bison/springboot/datacleanup/config/SpringBootDataCleanupAutoConfigurationTest.java
@@ -21,15 +21,15 @@ import com.commercetools.api.client.ProjectApiRoot;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import tech.bison.datacleanup.core.DataCleanup;
 
 @SpringBootTest(classes = {SpringBootDataCleanupAutoConfiguration.class})
 @ActiveProfiles("test")
 class SpringBootDataCleanupAutoConfigurationTest {
 
-  @MockBean
+  @MockitoBean
   private ProjectApiRoot projectApiRoot;
 
   @Autowired
